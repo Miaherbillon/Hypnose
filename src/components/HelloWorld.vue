2b771efb4b046ffadd1847155b2010d4741d4ccd
Miaherbillon <herbillon.mia@gmail.com>
2025-02-20 21:55:52 +0100

    suite 6

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,23 +10,40 @@ import fontImage from "/src/assets/Font.png";
 <style scoped>
 .title {
   width: 60%;
-  margin: 4vw auto;
-  padding: 5vh;
-  border-radius: 50vh;
+  margin: 2em auto;
+  padding: 1.5em;
+  border-radius: 50em;
   color: #000000;
   display: flex;
   justify-content: center;
+  align-items: center;
   background-color: white;
   box-sizing: border-box;
+  text-align: center;
 }
 h1 {
   box-sizing: border-box;
-  border: 5px white solid;
+  border: 0.3em solid white;
   border-radius: 50%;
   overflow: hidden;
   font-size: 3em;
-  box-shadow: 0 5px 15px 0px rgba(0, 0, 0, 0.6);
-  transform: translatey(0px);
+  text-align: center;
+  box-shadow: 0 0.3em 1em rgba(0, 0, 0, 0.6);
+  transform: translateY(0px);
   animation: float 6s ease-in-out infinite;
+}
+
+/* 📱 Responsive pour téléphones */
+@media (max-width: 768px) {
+  .title {
+    width: 80%;
+    padding: 1em 3em;
+    border-radius: 5em;
+  }
+
+  h1 {
+    font-size: 2em;
+    border-width: 0.2em;
+  }
 }
 </style>
--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -44,10 +44,9 @@ import { ref } from "vue";
         conviendrons ensemble d'un nouvel horaire.
       </p>
       <p>
-        <strong
-          >Prêt(e) à vous libérer du stress et à atteindre vos objectifs
-          ?</strong
-        >
+        <strong>
+          Prêt(e) à vous libérer du stress et à atteindre vos objectifs ?
+        </strong>
         <br />
         Contactez-moi pour prendre rendez-vous.
       </p>
@@ -56,35 +55,34 @@ import { ref } from "vue";
         Après validation du rendez-vous, vous recevrez un mail avec le lien de
         connexion Google Meet ainsi qu'un lien de paiement en ligne sécurisé.
         <p>
-          <strong class="S"
-            >Merci de vous connecter 5 minutes avant le début de la
-            séance.</strong
-          >
+          <strong class="S">
+            Merci de vous connecter 5 minutes avant le début de la séance.
+          </strong>
         </p>
       </div>
     </div>
   </div>
-
-  <br />
 </template>
 
 <style scoped>
+/* Styles de base */
 .P {
   width: 70%;
-  padding: 2vh 4vw;
+  padding: 2em 4em;
   margin: auto;
 }
 .D {
   color: rgb(210, 89, 194);
   display: flex;
   justify-content: center;
-  font-size: 2vh;
-  margin: 3vh;
+  font-size: 1.2em;
+  margin: 2em 0;
 }
 h2 {
   color: #000000;
-  -webkit-text-stroke: rgba(0, 0, 0, 0.229) 0.1vh;
-  font-size: 1.5em;
+  -webkit-text-stroke: rgba(0, 0, 0, 0.229) 0.08em;
+  font-size: 2em;
+  text-align: center;
 }
 strong {
   color: #000000;
@@ -95,13 +93,13 @@ strong {
   display: flex;
   justify-content: center;
   align-items: center;
-  border-radius: 5vh;
+  border-radius: 2em;
   text-align: center;
-  padding: 1vh;
-  margin: 3vh 20%;
+  padding: 1em;
+  margin: 2em auto;
   background-color: white;
   overflow: hidden;
-  box-shadow: 0 5px 15px 0px rgba(0, 0, 0, 0.6);
+  box-shadow: 0 0.5em 1.5em 0 rgba(0, 0, 0, 0.6);
   transform: translateY(0px);
   animation: float 6s ease-in-out infinite;
 }
@@ -111,10 +109,10 @@ strong {
   display: flex;
   justify-content: center;
   font-size: 1.5em;
-  margin: 4vh 0;
+  margin: 2em 0;
   transition: text-decoration 0.3s ease-in-out;
+  text-align: center;
 }
-
 .Q:hover {
   text-decoration: underline;
 }
@@ -125,5 +123,45 @@ strong {
 }
 .S {
   color: rgb(210, 89, 194);
+}
+
+/* 📱 Responsive pour les téléphones */
+@media (max-width: 768px) {
+  .P {
+    width: 80%;
+    margin: auto;
+  }
+
+  h2 {
+    font-size: 1.5em;
+  }
+
+  strong {
+    font-size: 1em;
+  }
+
+  .tarif {
+    width: 80%;
+    font-size: 1em;
+    padding: 1em;
+  }
+
+  .Q {
+    font-size: 1.2em;
+    margin: 1.5em 0;
+  }
+
+  .D {
+    font-size: 1em;
+    text-align: center;
+  }
+
+  ul {
+    padding-left: 1em;
+  }
+
+  li {
+    font-size: 1em;
+  }
 }
 </style>
